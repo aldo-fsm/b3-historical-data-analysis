993660425562587a41f2040c55c88aadd0b6f618
Aldo Monteiro <aldo.monteiro@pitang.com>
2021-02-03 19:23:10 -0300

    Load Dask Dataframe

--- data_vis.py
+++ data_vis.py
@@ -1,54 +1,29 @@
 import streamlit as st
-import pandas as pd
-import re
+import plotly_express as px
 
-with open('datasets/raw/COTAHIST_A2019.TXT') as f:
-    text = f.read()
+from datetime import datetime
+from datasets import loadDaskDataset, loadDataset, getDatasetName
 
-parseTable = pd.read_csv('datasets/parse-table.csv')
+def load():
+    time = datetime.now()
+    st.write(f'{time} - loading dataset...')
+    dataset = loadDaskDataset(
+        getDatasetName(2020)
+    )
+    time2 = datetime.now()
+    st.write(f'{time2} - loading finished')
+    st.write('Load time:', time2-time)
+    return dataset 
 
-allLines = text.split('\n')
-allLines[:10]
-allLines = allLines[1:-2]
-st.write(
-    len(allLines),
-    parseTable
-)
-# st.write(
-#     ([(i, line) for (i, line) in enumerate(allLines) if '99' in line[:2]]),
-#     ([(i, line) for (i, line) in enumerate(allLines) if '00' in line[:2]])
-# )
-def parseRecord(line: str):
-    data = {
-        row.fieldName : parseField(row.fieldName, row.type, line[int(row.startPos)-1:int(row.endPos)])
-        for i, row in parseTable.iterrows()
-    }
-    # data['DATA'] = parseDate(data['DATA'])
-    return data
-
-fieldsTypesMapping = dict(
-    dateFields=['DATA','DATVEN'],
-    intFields=['PRAZOT','TOTNEG','QUATOT','FATCOT'],
-    floatFields=['PREABE','PREMAX','PREMIN','PREMED','PREULT','PREOFC','PREOFV','VOLTOT','PREEXE','PTOEXE'],
-    stringFields=['TIPREG','CODBDI','CODNEG','TPMERC','NOMRES','ESPECI','MODREF','INDOPC','CODISI','DISMES'],
-)
-
-def parseField(fieldName: str, fieldType: str, value: str):
-    if fieldName in fieldsTypesMapping['dateFields']:
-        return parseDate(value)
-    elif fieldName in fieldsTypesMapping['intFields']:
-        try:
-            return int(value)
-        except:
-            return None
-    elif fieldName in fieldsTypesMapping['floatFields']:
-        integerDigitNumber = int(re.findall('\((\d+)\)', fieldType)[0])
-        return float(f'{value[:integerDigitNumber]}.{value[integerDigitNumber:]}')
-    return value.strip()
-
-def parseDate(date: str):
-    return f"{date[0:4]}-{date[4:6]}-{date[6:8]}"
+dataset = load()
 
 st.write(
-    [parseRecord(line) for line in allLines[:5]]
+    # dataset.size.compute(),
+    dataset.head(),
+    dataset[dataset.CODNEG == 'ABBV3'].head()
+)
+
+tickers = st.multiselect('CODNEG', dataset.CODNEG.unique().compute())
+st.write(
+    px.line(dataset[dataset.CODNEG.isin(tickers)].compute().sort_values('DATA'), x='DATA', y='PREABE', color='CODNEG')
 )
