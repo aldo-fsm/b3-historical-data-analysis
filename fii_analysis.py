7b5d2b73213a49ab4c9f8b3f3c6efa2dd7e58c1b
Aldo Monteiro <aldo.monteiro@pitang.com>
2021-12-01 11:58:17 -0300

    Cut yields list and filter by number of assets

--- fii_analysis.py
+++ fii_analysis.py
@@ -2,6 +2,7 @@ import streamlit as st
 from scrapping import scraper_fundsexplorer
 import pandas as pd
 import numpy as np
+import plotly_express as px
 
 @st.cache()
 def getData():
@@ -19,6 +20,10 @@ fiis['liquidez'] = fiis.apply(lambda row: row['Preço Atual'] * row['Liquidez Di
 
 fiis['yields'] = fiis['Código do fundo'].apply(lambda ticker: yields[yields.ticker == ticker].value.values.T)
 fiis['yieldsDates'] = fiis['Código do fundo'].apply(lambda ticker: yields[yields.ticker == ticker].date.values.T)
+
+MAX_NUM_MONTHS = 24
+fiis['yields'] = fiis.yields.apply(lambda yields: yields[-MAX_NUM_MONTHS:])
+fiis['yieldsDates'] = fiis.yieldsDates.apply(lambda yieldsDates: yieldsDates[-MAX_NUM_MONTHS:])
 
 fiis['yieldMean'] = fiis.yields.apply(lambda data: data.mean())
 fiis['yieldMedian'] = fiis.yields.apply(lambda data: np.median(data))
@@ -54,6 +59,8 @@ filtered = filtered[filtered.liquidez >= 200000] # liquidez >= R$ 200.000
 filtered = filtered[filtered.meanMedianDiscrepance <= 0.1] # baixa discrepância entre média e mediana do DY
 filtered = filtered[filtered.numYieldDataPoints >= 12] # pelo menos 1 ano de dados dividend yield
 
+filtered = filtered[filtered['Quantidade Ativos'] > 1]
+
 filtered['pvpaRankPosition'] = np.argsort(np.argsort(filtered['P/VPA'].values))
 filtered['yieldMedianRankPosition'] = np.argsort(np.argsort(-filtered.yieldMedian.values))
 filtered['srankPosition'] = filtered.apply(lambda row: row.pvpaRankPosition + row.yieldMedianRankPosition, axis=1)
@@ -66,3 +73,7 @@ st.write(
     ranked,
 )
 
+ranked['negYieldMedian'] = -ranked.yieldMedian
+st.write(
+    px.scatter(ranked, x = 'P/VPA', y = 'negYieldMedian', hover_name='Código do fundo')
+)
